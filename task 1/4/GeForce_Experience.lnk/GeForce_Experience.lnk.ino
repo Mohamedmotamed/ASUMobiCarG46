8fe2f09f27c3740f9ce64af778e49cb81f482ff1
Mohamed Khaled <mohamed30997@gmail.com>
2018-02-19 23:11:30 +0200

    adding some files

---
+++ task 1/4/GeForce_Experience.lnk/GeForce_Experience.lnk.ino
@@ -0,0 +1,14 @@
+#define led 13
+void setup() {
+  // put your setup code here, to run once:
+pinMode (led , OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (led,1);
+delay (500);
+digitalWrite (led,0);
+delay (1500);
+
+}
