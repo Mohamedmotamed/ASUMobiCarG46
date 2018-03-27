75c8b38775b72873ae0d0addca3f33d4b4c301ce
ramzy7779 <31419509+ramzy7779@users.noreply.github.com>
2018-03-27 11:21:32 +0200

    testing for timing lib

--- auto driving of ultrasonic/desktop.ini/desktop.ini.ino
+++ auto driving of ultrasonic/desktop.ini/desktop.ini.ino
@@ -1,3 +1,6 @@
+#include <elapsedMillis.h>
+elapsedMillis timeElapsed=0;
+unsigned int interval = 1500;
 int led = 13;
 int pin6=8;//B2
 int pin7=9;//B1
@@ -106,7 +109,12 @@ digitalWrite(pin9,LOW);
   { 
     red_state=digitalRead(red);
     green_state=digitalRead(green);
-    if ((distance>=18) && (distance<=35)) //care about range from 18cm to 35cm
+    if ((distance>0) && (distance<=11))
+    { digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);}
+    if ((distance>11) && (distance<=30)) //care about range from 18cm to 35cm
     {digitalWrite(red,HIGH); //will detect with a red led
     digitalWrite(green,LOW);
     delay(100);            //just to stable any change
@@ -123,20 +131,19 @@ digitalWrite(pin9,LOW);
      }
      while (red_state==HIGH);     //this loop will stay excuting as long as our detector (red led) is high
     }
-     else if ((distance>35) && (distance<=50))  //another range from more than 35cm to 50cm
-     {digitalWrite(green,HIGH);   //here green led is the detector
+     else if ((distance>30) && (distance<=50))  //another range from more than 35cm to 50cm
+     {
+      timeElapsed=0; //to reset the counter to 0 before entering the loop
+      digitalWrite(green,HIGH);   //here green led is the detector
     digitalWrite(red,LOW);
-     delay(100);            //just to stable any change
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW); // 4 orders to stop the car
-     delay (250);
+    delay(100);
+     while (timeElapsed <= interval)
+     {
     digitalWrite(pin6,HIGH);   
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
-     digitalWrite(pin9,HIGH);   //these 4 orders refering to turn right
-     delay (1500);    //refer to the required time to make car turn right completely -----> measured from video
+     digitalWrite(pin9,HIGH); }  //these 4 orders refering to turn right
+         //this loop refering to the required time to make car turn right completely -----> measured from video=1.5s
      do {digitalWrite(pin6,HIGH);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW); 
@@ -146,7 +153,24 @@ digitalWrite(pin9,LOW);
      }
   
    else
-   { digitalWrite(green, HIGH);
+   {timeElapsed=0;
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);
+     delay (150);
+       while (timeElapsed <= interval){
+     digitalWrite(pin6,LOW);
+    digitalWrite(pin7,HIGH);
+    digitalWrite(pin8,HIGH);
+    digitalWrite(pin9,LOW);}
+    
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);
+     delay (150);
+    digitalWrite(green, HIGH);
     digitalWrite(red, HIGH); //2 leds here detect that the car in a zone from 50cm to 2m (existance of barrier)
 
       while(BluetoothData=='L')
