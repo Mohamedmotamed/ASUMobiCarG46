bbd0b8171461316bbbac92b358f21bcdea80bcc5
AreegMohamed <36485296+AreegMohamed@users.noreply.github.com>
2018-03-29 22:57:18 +0200

    1

---
+++ ar1.ino
@@ -0,0 +1,16 @@
+//example01
+const int LED=12;
+void setup() {
+  pinMode(LED,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(LED,HIGH);
+  Delay(500);
+  Digitalwrite(LED,LOW);
+  DELAY(500);
+  // put your main code here, to run repeatedly:
+
+}
