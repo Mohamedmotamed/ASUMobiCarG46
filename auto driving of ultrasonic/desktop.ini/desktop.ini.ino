fddcd799ca0dd03f94d51038a3981d9036d1f34c
ramzy7779 <31419509+ramzy7779@users.noreply.github.com>
2018-03-29 13:51:55 +0200

    easy &auto code
    the auto driving of ultrasonic is working probably but in easy mode it working with delay before excute any order

--- auto driving of ultrasonic/desktop.ini/desktop.ini.ino
+++ auto driving of ultrasonic/desktop.ini/desktop.ini.ino
@@ -1,8 +1,7 @@
 #include <elapsedMillis.h>
 elapsedMillis timeElapsed=0;
 unsigned int interval = 1500;
-unsigned int interval2 = 3000;
-unsigned int interval3 = 4500;
+
 int led = 13;
 int pin6=8;//B2
 int pin7=9;//B1
@@ -34,8 +33,11 @@ Serial.begin(9600);
 
 void loop() {
 
+ 
 
- digitalWrite(trigPin, LOW);
+
+
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -46,129 +48,10 @@ void loop() {
   Serial.print("Distance: ");
   Serial.println(distance);
   delay(100);
-  
-    
-    
-    
-    //this part of code is responsible on driving a car automaically without andorid app to avoid obstacles
-
-
- red_state=digitalRead(red);
-    green_state=digitalRead(green);
- 
-  
-  if ((distance>=0)&&(distance<55)) //55cm btw ultra sensor and barrier
-  { 
-    
-   if ((distance>0) && (distance<=11))
-    {digitalWrite(red,HIGH); 
-    digitalWrite(green,HIGH);
-    delay (200);
-    digitalWrite(red,LOW); 
-    digitalWrite(green,LOW);
-                               //detect with flashing
-      digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW);}
-    else if ((distance>11) && (distance<=27.2)) //care about range from 11cm to 27.2cm
-    {digitalWrite(red,HIGH); //will detect with a red led
-    digitalWrite(green,LOW);
-    delay(100);            //just to stable any change
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW); // 4 orders to stop the car
-     delay (250);
-     if (red_state==HIGH) {
-      digitalWrite(pin6,LOW);
-     digitalWrite(pin7,HIGH);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,HIGH);  //refering to back direction
-     }
-          //this loop will stay excuting as long as our detector (red led) is high
-    }
-     else if ((distance>27.2) && (distance<=40))  //another range from more than 27.2cm to 40cm
-     {
-      timeElapsed=0; //to reset the timer to 0 before entering the loop
-      digitalWrite(green,HIGH);   //here green led is the detector
-    digitalWrite(red,LOW);
-    delay(100);
-     if (timeElapsed <= interval)
-     {
-    digitalWrite(pin6,HIGH);   
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,HIGH); }  //these 4 orders refering to turn right
-         //this loop refering to the time required  to make car turn right completely -----> measured from video=1.5s
-     if ((green_state==HIGH)&& (timeElapsed > interval)) {
-      digitalWrite(pin6,HIGH);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW); 
-     digitalWrite(pin9,HIGH); //4 orders refering to forward direction
-     timeElapsed=0;
-     }
-      //this loop will stay excuting as long as our detector (green led) is high
-     }
-  
-   else
-   {if (timeElapsed > interval){
-    
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW);
-     delay (150);
-      digitalWrite(green, HIGH);
-      digitalWrite(red, HIGH); //2 leds here detect that the car in a region from 40cm to 55cm (existance of barrier)
-
-       if (timeElapsed <= interval2){
-     digitalWrite(pin6,LOW);
-    digitalWrite(pin7,HIGH);
-    digitalWrite(pin8,HIGH);
-    digitalWrite(pin9,LOW);}
-    
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW);
-     delay (150);
-   }}
-  }    else {
-    digitalWrite(red,LOW); 
-    digitalWrite(green,LOW);
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW);
-     delay (150);
-     if ((timeElapsed >= interval)&& (timeElapsed <= interval2)) {
-      digitalWrite(red,LOW); 
-    digitalWrite(green,HIGH); //acting like stage 3
-    
-      digitalWrite(pin6,HIGH);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW); 
-     digitalWrite(pin9,HIGH); //4 orders refering to forward direction
-     }
-       else if ((timeElapsed >= interval2)&&(timeElapsed <= interval3)){
-          digitalWrite(red,HIGH); 
-    digitalWrite(green,HIGH); //acting like stage 4
-    
-     digitalWrite(pin6,LOW);
-    digitalWrite(pin7,HIGH);
-    digitalWrite(pin8,HIGH);
-    digitalWrite(pin9,LOW);}  //left direction
-    
-    digitalWrite(pin6,LOW);
-     digitalWrite(pin7,LOW);
-     digitalWrite(pin8,LOW);
-     digitalWrite(pin9,LOW);
-     delay (150);
-   timeElapsed=0;
-     
-    
-if ((Serial.available())&&(timeElapsed <= interval)){
+    timeElapsed=0;
+       
+        if ((Serial.available())&&(distance>=55)){
+      
 
 BluetoothData=Serial.read();
 
@@ -216,16 +99,133 @@ digitalWrite(pin6,LOW);
 digitalWrite(pin7,LOW);
 digitalWrite(pin8,LOW);
 digitalWrite(pin9,LOW);
-}
+}}
+    
+    
+    
+    
+    //this part of code is responsible on driving a car automaically without andorid app to avoid obstacles
 
 
-}
+
+
+  
+  else  if ((distance>=0)&&(distance<55)) //55cm btw ultra sensor and barrier
+  { 
+    
+   if ((distance>0) && (distance<=11))  //(stage 1)
+    {digitalWrite(red,HIGH); 
+    digitalWrite(green,HIGH);
+    delay (200);
+    digitalWrite(red,LOW); 
+    digitalWrite(green,LOW);
+                               //detect with flashing
+      digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);}
+    else if ((distance>11) && (distance<=27.2)) //care about range from 11cm to 27.2cm ---->(stage 2)
+    {digitalWrite(red,HIGH); //will detect with a red led
+    digitalWrite(green,LOW);
+    delay(100);            //just to stable any change
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW); // 4 orders to stop the car
+     delay (250);
+     if (red_state==HIGH) {
+      digitalWrite(pin6,LOW);
+     digitalWrite(pin7,HIGH);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,HIGH);  //refering to back direction
+     }
+          //this loop will stay excuting as long as our detector (red led) is high
+    }
+     else if ((distance>27.2) && (distance<=40))  //another range from more than 27.2cm to 40cm ----->(stage 3)
+     {
+      timeElapsed=0; //to reset the timer to 0 before entering the loop
+      digitalWrite(green,HIGH);   //here green led is the detector
+    digitalWrite(red,LOW);
+    delay(100);
+     while (timeElapsed <= interval)
+     {
+    digitalWrite(pin6,HIGH);   
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,HIGH); }  //these 4 orders refering to turn right
+         //this loop refering to the time required  to make car turn right completely -----> measured from video=1.5s
+         timeElapsed=interval;
+     if ((green_state==HIGH)&& (timeElapsed == interval)) {
+      digitalWrite(pin6,HIGH);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW); 
+     digitalWrite(pin9,HIGH); //4 orders refering to forward direction
+     timeElapsed=0;
+     }
+      //this loop will stay excuting as long as our detector (green led) is high
+     }
+  
+   else                             //stage 4
+   {if (timeElapsed <= interval){
+    
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);  //stop forwarding from previous stage
+     delay (150);
+      digitalWrite(green, HIGH);
+      digitalWrite(red, HIGH); //2 leds here detect that the car in a region from 40cm to 55cm (existance of barrier)
+
+       while (timeElapsed < interval){
+     digitalWrite(pin6,LOW);
+    digitalWrite(pin7,HIGH);
+    digitalWrite(pin8,HIGH);
+    digitalWrite(pin9,LOW);
+    }                           //to turn left copmpletely (90 degree)
+    
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);
+     delay (150);
+   }}
+  }    else {
+    digitalWrite(red,LOW); 
+    digitalWrite(green,LOW);
+    digitalWrite(pin6,LOW);
+     digitalWrite(pin7,LOW);
+     digitalWrite(pin8,LOW);
+     digitalWrite(pin9,LOW);
+     delay (150);
+     if ((timeElapsed >= interval)&& (timeElapsed < 2* interval))
+     {
+          digitalWrite(red,LOW); 
+          digitalWrite(green,HIGH); //acting like stage 3
+          while (timeElapsed <= 2*interval)
+         {
+          digitalWrite(pin6,HIGH);
+          digitalWrite(pin7,LOW);
+          digitalWrite(pin8,LOW); 
+          digitalWrite(pin9,HIGH);  //4 orders refering to forward direction
+         } 
+     }
+       else if ((timeElapsed >=2* interval)&&(timeElapsed < 3*interval)){
+          digitalWrite(red,HIGH); 
+          digitalWrite(green,HIGH); //acting like stage 4
+          while (timeElapsed <= 3*interval)
+          {
+          digitalWrite(pin6,LOW);
+          digitalWrite(pin7,HIGH);
+          digitalWrite(pin8,HIGH);
+          digitalWrite(pin9,LOW);}  //left direction
+       }
       
-    }
+   }
+       
     
     
     
-    }
+     }
 
 
 
