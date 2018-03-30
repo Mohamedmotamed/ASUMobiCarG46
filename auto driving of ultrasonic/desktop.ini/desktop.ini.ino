62005802bcb70566106912a91d1af40316b67e98
ramzy7779 <31419509+ramzy7779@users.noreply.github.com>
2018-03-30 16:16:28 +0200

    anything

--- auto driving of ultrasonic/desktop.ini/desktop.ini.ino
+++ auto driving of ultrasonic/desktop.ini/desktop.ini.ino
@@ -1,7 +1,7 @@
 #include <elapsedMillis.h>
 elapsedMillis timeElapsed=0;
 unsigned int interval = 1500;
-
+int i;
 int led = 13;
 int pin6=8;//B2
 int pin7=9;//B1
@@ -14,8 +14,8 @@ int green=4;
 int red_state;
 int green_state;
 float duration, distance;
-const int ex=55;
 char BluetoothData;
+
 
 void setup() {
 pinMode(led,OUTPUT);
@@ -57,7 +57,7 @@ BluetoothData=Serial.read();
 
 
 if(BluetoothData=='L'){ 
-
+Serial.println("left");
 
 digitalWrite(pin6,LOW);
 digitalWrite(pin7,HIGH);
@@ -67,7 +67,7 @@ digitalWrite(pin9,LOW);
 }
 
 else if (BluetoothData=='R'){
-
+Serial.println("right");
 digitalWrite(pin6,HIGH);
 digitalWrite(pin7,LOW);
 digitalWrite(pin8,LOW);
@@ -77,7 +77,7 @@ digitalWrite(pin9,HIGH);
 }
 else if(BluetoothData=='F')
 {
-
+Serial.println("frward");
 digitalWrite(pin6,HIGH);
 digitalWrite(pin7,LOW);
 digitalWrite(pin8,HIGH);
@@ -86,7 +86,7 @@ digitalWrite(pin9,LOW);
 }
 else if (BluetoothData=='B')
 {
-
+Serial.println("back");
 digitalWrite(pin6,LOW);
 digitalWrite(pin7,HIGH);
 digitalWrite(pin8,LOW);
@@ -94,7 +94,7 @@ digitalWrite(pin9,HIGH);
 }
 else
 {
-
+Serial.println("stop");
 digitalWrite(pin6,LOW);
 digitalWrite(pin7,LOW);
 digitalWrite(pin8,LOW);
@@ -119,7 +119,7 @@ digitalWrite(pin9,LOW);
     delay (200);
     digitalWrite(red,LOW); 
     digitalWrite(green,LOW);
-                               //detect with flashing
+      Serial.println("auto stop range 1");                         //detect with flashing
       digitalWrite(pin6,LOW);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
@@ -128,12 +128,14 @@ digitalWrite(pin9,LOW);
     {digitalWrite(red,HIGH); //will detect with a red led
     digitalWrite(green,LOW);
     delay(100);            //just to stable any change
+    Serial.println("auto stop range 2"); 
     digitalWrite(pin6,LOW);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
      digitalWrite(pin9,LOW); // 4 orders to stop the car
      delay (250);
      if (red_state==HIGH) {
+      Serial.println("auto back range 2"); 
       digitalWrite(pin6,LOW);
      digitalWrite(pin7,HIGH);
      digitalWrite(pin8,LOW);
@@ -147,27 +149,37 @@ digitalWrite(pin9,LOW);
       digitalWrite(green,HIGH);   //here green led is the detector
     digitalWrite(red,LOW);
     delay(100);
-     while (timeElapsed <= interval)
+    for ( i=0;i<1;i++){
+   while (timeElapsed<=interval)
      {
+      Serial.println("auto right range 3");
+      Serial.println( "timeElapsed=");
+      Serial.println( timeElapsed);
     digitalWrite(pin6,HIGH);   
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
-     digitalWrite(pin9,HIGH); }  //these 4 orders refering to turn right
+     digitalWrite(pin9,HIGH); 
+     
+      } }//these 4 orders refering to turn right
          //this loop refering to the time required  to make car turn right completely -----> measured from video=1.5s
-         timeElapsed=interval;
-     if ((green_state==HIGH)&& (timeElapsed == interval)) {
+        timeElapsed=1500;
+     Serial.println( "finally");
+    
+    while (red_state==HIGH) {
+     if ( timeElapsed==1500) {
+      Serial.println("auto forward range 3"); 
       digitalWrite(pin6,HIGH);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW); 
      digitalWrite(pin9,HIGH); //4 orders refering to forward direction
-     timeElapsed=0;
-     }
+     
+     }}timeElapsed=0;
       //this loop will stay excuting as long as our detector (green led) is high
      }
   
    else                             //stage 4
    {if (timeElapsed <= interval){
-    
+    Serial.println("auto stop range 4"); 
     digitalWrite(pin6,LOW);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
@@ -177,12 +189,13 @@ digitalWrite(pin9,LOW);
       digitalWrite(red, HIGH); //2 leds here detect that the car in a region from 40cm to 55cm (existance of barrier)
 
        while (timeElapsed < interval){
+        Serial.println("auto left range 4"); 
      digitalWrite(pin6,LOW);
     digitalWrite(pin7,HIGH);
     digitalWrite(pin8,HIGH);
     digitalWrite(pin9,LOW);
     }                           //to turn left copmpletely (90 degree)
-    
+    Serial.println("auto stop range 4"); 
     digitalWrite(pin6,LOW);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
@@ -190,35 +203,42 @@ digitalWrite(pin9,LOW);
      delay (150);
    }}
   }    else {
+   
     digitalWrite(red,LOW); 
     digitalWrite(green,LOW);
+     Serial.println("auto stop range 5"); 
     digitalWrite(pin6,LOW);
      digitalWrite(pin7,LOW);
      digitalWrite(pin8,LOW);
      digitalWrite(pin9,LOW);
      delay (150);
-     if ((timeElapsed >= interval)&& (timeElapsed < 2* interval))
+     if ((timeElapsed >= interval)&& (timeElapsed < (interval+interval)))
      {
           digitalWrite(red,LOW); 
           digitalWrite(green,HIGH); //acting like stage 3
-          while (timeElapsed <= 2*interval)
+          while (timeElapsed <= (interval+interval))
          {
+           Serial.println("auto forward range 5.5"); 
           digitalWrite(pin6,HIGH);
           digitalWrite(pin7,LOW);
           digitalWrite(pin8,LOW); 
           digitalWrite(pin9,HIGH);  //4 orders refering to forward direction
          } 
+         timeElapsed=3000;
      }
-       else if ((timeElapsed >=2* interval)&&(timeElapsed < 3*interval)){
+       else if ((timeElapsed >=(interval+interval))&&(timeElapsed < (interval+interval+interval))){
           digitalWrite(red,HIGH); 
           digitalWrite(green,HIGH); //acting like stage 4
           while (timeElapsed <= 3*interval)
           {
+             Serial.println("auto lefe range 5.5"); 
           digitalWrite(pin6,LOW);
           digitalWrite(pin7,HIGH);
           digitalWrite(pin8,HIGH);
-          digitalWrite(pin9,LOW);}  //left direction
+          digitalWrite(pin9,LOW); }  //left direction
+           timeElapsed=0;
        }
+       
       
    }
        
