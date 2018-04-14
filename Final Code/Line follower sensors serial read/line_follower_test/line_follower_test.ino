26f5ff32919844536ec82548f13ce4e037bda84c
mohamedyahiasan <mohamedyoyo55@gmail.com>
2018-04-15 00:18:52 +0200

    1

--- Final Code/Line follower sensors serial read/line_follower_test/line_follower_test.ino
+++ Final Code/Line follower sensors serial read/line_follower_test/line_follower_test.ino
@@ -1,3 +1,9 @@
+#include <elapsedMillis.h>
+
+
+
+//motor A
+
 #define in1 8
 #define in2 9
 //motor b
@@ -5,9 +11,19 @@
 #define in3 10
 #define in4 11
 int r, c, l ;
-
+const int trigPin = 2;
+const int echoPin = 6;
+int led = 13;
+int S1=5 , S2=3;//Capital S
+long duration;
+int distance;
+char estado ;
+unsigned int interval = 1000;
+  elapsedMillis timeElapsed;
 void setup() {
 Serial.begin(9600);
+pinMode(S1,OUTPUT);
+pinMode(S2,OUTPUT);
 pinMode(A3,INPUT);
   pinMode(A4,INPUT);
   pinMode(A5,INPUT);
@@ -15,11 +31,28 @@ pinMode(A3,INPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  pinMode(led, OUTPUT);
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
 }
-
-
 void loop() {
-  l=1000-analogRead(A3);
+// Clears the trigPin
+/*digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+digitalWrite(led,LOW);*/
+l=1000-analogRead(A3);
 c=analogRead(A4);
 r=analogRead(A5);
  Serial.print("l  ");
@@ -29,54 +62,90 @@ r=analogRead(A5);
   Serial.print("r  ");
   Serial.println(analogRead(A5));
 
-   if(c > 700 && r < 700 && l < 700)
-  {
-    forward();
-  }
- else if(r > 700 && c < 700 && l < 700)
-  {
-  right();
-  }
-  
 
-  else if(l > 700 && c<700 && r <700)
-  {
-   left();
-   }
- else if ( l + r +c >2100)
- {
-  forward();
-  }
-  else 
-  {
-    Stop();
 
- }
   
+/*if(Serial.available()>0){ 
+estado = Serial.read();
+Serial.println("bt " + estado);
+} 
+
+if(estado=='L'){
+ Max();
+left ();
+
 }
 
+else if (estado=='R'){
+  Max();
+right ();
+ 
+}
+else if(estado=='F')
+{
+  Max();
+forward ();
 
+if (distance<=15)
+ {
 
+   
+   while(timeElapsed<interval)
+   {
+   digitalWrite(13, HIGH);
+  back();
+   }
+   timeElapsed=0;
+    digitalWrite(led,LOW);
+ Stop();
+  
+  
 
+  }
+}
+else if (estado=='B')
+{
+  Max();
+back();
+}
+else if (estado=='V')
+{*/
+  
+lf ();
 
+/*  }
 
-
-
+else 
+{
+  Stop();
+  }*/
+}
+ 
+  
+  
+void Max()
+{
+  analogWrite(S1,255);
+  analogWrite(S2,255);
+  }
 
   void forward ()
     {
+    
      digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);}
       void back ()
     {
+     
       digitalWrite(in1, LOW);
   digitalWrite(in2,HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);}
       void right ()
-    {Serial.println("right");
+    {
+      
       digitalWrite(in1, HIGH);
  digitalWrite(in2, LOW);
  digitalWrite(in3, LOW);
@@ -85,6 +154,7 @@ r=analogRead(A5);
  
       void left ()
  {
+
      digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
@@ -100,4 +170,40 @@ r=analogRead(A5);
 
  
   
+ void lf ()
+  {
+    analogWrite(S1,170);
+    analogWrite(S2,170);
+      if(c > 700 && r < 700 && l < 700)
+  {
+    forward();
+  }
+ else if(r > 700 && c < 700 && l < 700)
+  {
+   digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, LOW);
+  }
+  
 
+  else if(l > 700 && c<700 && r <700)
+  {
+     digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+   }
+ else if ( l + r +c >2100)
+ {
+  forward();
+  }
+  else 
+  {
+     digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, LOW);
+ }
+  
+  }
